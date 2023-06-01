014b1278499263023da8b76e3c4ede7abe2b10e6
Raul Bártolo <rbartolo@maiseficaz.pt>
2023-06-01 12:13:19 +0100

    added models

---
+++ src/main/java/com/example/ClassesBookings/model/Class.java
@@ -0,0 +1,56 @@
+package com.example.ClassesBookings.model;
+
+import java.util.ArrayList;
+
+public class Class {
+    private String name;
+    private String date;
+    private int capacity;
+
+
+    private ArrayList<Member> bookings;
+
+    public Class(){
+
+    }
+
+    public Class(String name, String date, int capacity){
+        this.name = name;
+        this.date = date;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public ArrayList<Member> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(ArrayList<Member> bookings) {
+        this.bookings = bookings;
+    }
+
+
+}
---
+++ src/main/java/com/example/ClassesBookings/model/Member.java
@@ -0,0 +1,22 @@
+package com.example.ClassesBookings.model;
+
+public class Member {
+
+    private String name;
+
+    public Member(){
+
+    }
+
+    public Member(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
