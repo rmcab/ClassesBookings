a06fbc6d2349f81084333d7bef033cda6682936d
Raul Bártolo <rbartolo@maiseficaz.pt>
2023-06-01 11:52:01 +0100

    Initial commit

---
+++ src/test/java/com/example/ClassesBookings/ClassesBookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClassesBookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesBookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
