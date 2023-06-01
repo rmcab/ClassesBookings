e0c182857d88e9754bb2c0b8e81760dc201f729a
Raul Bártolo <raul.almeida.bartolo@gmail.com>
2023-06-01 23:33:24 +0100

    swagger implementation

--- src/main/java/com/example/ClassesBookings/controller/GymClassController.java
+++ src/main/java/com/example/ClassesBookings/controller/GymClassController.java
@@ -1,11 +1,17 @@
 package com.example.ClassesBookings.controller;
 import com.example.ClassesBookings.model.GymClass;
 import com.example.ClassesBookings.service.GymClassService;
+import com.example.ClassesBookings.utils.Utils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import java.util.List;
 
@@ -16,9 +22,19 @@ public class GymClassController {
     @Autowired
     private GymClassService service;
 
+    @Operation(summary = "Searches existing classes with optional parameters"
+            ) /*notes = "Returns a list of classes"*/
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - The product was not found")
+    })
+
     @GetMapping
     public List<GymClass> getClasses(@RequestParam(name="name",required=false) String name, @RequestParam(name="date",required=false) String date){
-        //needs to validate date format; If date format is invalid give error message;
+
+        /*if(Utils.isNotNullOrEmptyString(date) && !Utils.isValidDate(date)){
+
+        }*/
 
         return service.findClasses(name, date);
     }
--- src/main/java/com/example/ClassesBookings/utils/Utils.java
+++ src/main/java/com/example/ClassesBookings/utils/Utils.java
@@ -1,8 +1,22 @@
 package com.example.ClassesBookings.utils;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Utils {
 
     public static boolean isNotNullOrEmptyString(String s){
         return s != null && !"".equals(s);
+    }
+
+    public static boolean isValidDate (String dateString){
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("mm/dd/yyyy");
+        dateFormatter.setLenient(false);
+        Date date = null;
+        try{
+            date = dateFormatter.parse(dateString);
+        } catch(Exception e){
+
+        }
+        return date != null;
     }
 }
